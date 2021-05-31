c7c459aa5a6d221b3906c3dc8613a1fc06e80b31
theponz05 <71616948+theponz05@users.noreply.github.com>
2021-05-30 21:47:27 -0400

    working
    need to eliminate the use of the for loop, though

--- src/js/App.js
+++ src/js/App.js
@@ -6,11 +6,31 @@ let html = '';
 async function currency(){
     const res = await fetch(API_URL);
     const data = await res.json();
-    console.log(data.rates)
     const arrayData = Object.keys(data.rates);
+    const rates = data.rates;
     console.log(arrayData)
+    console.log(rates)
     arrayData.map(item =>{
         return html += `<option value=${item}>${item}</option>`;
+    });
+    for(let i=0; i<select.length; i++){
+        select[i].innerHTML = html;
+    };
+
+    input[0].addEventListener('keyup', ()=>{
+        input[1].value = input[0].value * rates[select[1].value] / rates[select[0].value];
+    });
+    
+    input[1].addEventListener('keyup', ()=>{
+        input[0].value = input[1].value * rates[select[0].value] / rates[select[1].value];
+    });
+    
+    select[0].addEventListener('change', ()=>{
+        input[1].value = input[0].value * rates[select[1].value] / rates[select[0].value];
+    });
+    
+    select[1].addEventListener('change', ()=>{
+        input[0].value = input[1].value * rates[select[0].value] / rates[select[1].value];
     });
 
 };
